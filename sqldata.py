8acbc13c9d29d77141c25b6b75d545172377c898
fjort <daguoochen@gmail.com>
2020-05-09 22:10:50 +0800

    Add sqlite module.

--- sqldata.py
+++ sqldata.py
@@ -2,10 +2,44 @@
 
 import sqlite3
 
-con = sqlite3.connect('sqlite.db')
+con = sqlite3.connect('maze.db')
+
+def create_tables():
+
 
 c = con.cursor()
-c.execute('''CREATE TABLE CONFIG
-        (ID TEXT PRIMARY KEY NOT NULL,
-        )
-    ''')
+# c.execute('''CREATE TABLE CONFIG
+#         (ID TEXT PRIMARY KEY NOT NULL,
+#         NAME TEXT NOT NULL,
+#         AGE INT NOT NULL,
+#         ADDRESS CHAR(50),
+#         SALARY REAL);''')
+# print('Table created successfully.')
+# con.commit()
+# con.close()
+
+# c.execute("INSERT INTO CONFIG (ID, NAME, AGE, ADDRESS, SALARY) \
+#           VALUES(1, 'Paul', 32, 'California', 20000.00)")
+#
+# c.execute("INSERT INTO CONFIG (ID, NAME, AGE, ADDRESS, SALARY) \
+#           VALUES(2, 'Allen', 25, 'Texas', 15000.00)")
+#
+# c.execute("INSERT INTO CONFIG (ID,NAME,AGE,ADDRESS,SALARY) \
+#       VALUES (3, 'Teddy', 23, 'Norway', 20000.00 )")
+#
+# c.execute("INSERT INTO CONFIG (ID,NAME,AGE,ADDRESS,SALARY) \
+#       VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 )")
+
+c.execute('UPDATE CONFIG SET SALARY = 55000.00 WHERE ID = 1')
+con.commit()
+
+
+cursor = con.execute("SELECT id, name, address, salary  from CONFIG")
+for row in cursor:
+    print("ID = ", row[0])
+    print("NAME = ", row[1])
+    print("ADDRESS = ", row[2])
+    print("SALARY = ", row[3], "\n")
+
+print('Records created successfully')
+con.close()
